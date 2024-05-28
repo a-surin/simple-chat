e73d02276b3b16434dc8656937f7370968efbe16
Сурин Алексей <surin_a_a@forabank.ru>
2024-05-28 21:39:29 +0300

    init

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        // создаем серверный сокет на порту 1234
+        ServerSocket server = new ServerSocket(1234);
+        while(true) {
+            System.out.println("Waiting...");
+            // ждем клиента из сети
+            Socket socket = server.accept();
+            System.out.println("Client connected!");
+            // создаем клиента на своей стороне
+            new Thread (new Client(socket)).start();
+        }
+     }
+}
